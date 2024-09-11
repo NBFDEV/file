13285306e042c879fcd6079e7fb35bd7333683c2
Narawit Suraphan <141490518+SHL-X@users.noreply.github.com>
2024-09-11 10:57:45 +0700

    Add files via upload

---
+++ show.bat
@@ -0,0 +1,20 @@
+@echo off
+python -c "import pyautogui as pag; pag.click(785, 17, duration=2)"
+python -c "import pyautogui as pag; pag.click(903, 64, duration=2)"
+start "" /MAX "C:\Users\Public\Desktop\VMQuickConfig"
+python -c "import pyautogui as pag; pag.click(147, 489, duration=2)"
+python -c "import pyautogui as pag; pag.click(156, 552, duration=2)"
+python -c "import pyautogui as pag; pag.click(587, 14, duration=2)"
+echo ..........................................................
+echo .....พัฒนาโดย Narawit Dev.................................
+echo  ____   ____   ____  ______ ------------------------------
+echo / ___| / ___| / ___||  ____|------------------------------
+echo \___ \ \___ \ \___ \|  _|   ------------------------------
+echo  ___) | ___) |___) | |____ -------------------------------
+echo |____/ |____/|____/|______|-------------------------------
+echo ..........................................................
+echo ------------------- Narawit Suraphub ---------------------
+echo ..........................................................
+echo ........ Subscribe To Our Official Method Channel ........
+echo ..........................................................
+echo Your Device Name: %username%@%computername%
